44e880941e78866c3db757002cb8f371ff7f8a7e
Alessandra Davila <davilaaguilar23@gmail.com>
2016-05-03 10:12:43 -0500

    reflects correct total for each item after discount

--- public/js/controllers/CartCtrl.js
+++ public/js/controllers/CartCtrl.js
@@ -7,18 +7,24 @@ app.controller('CartCtrl', ['$scope', function($scope){
             discounted = item.quantity - 1;
             discount = (discounted / 2) + 1;
             sum += (item.price * discount);
+            item.total = (item.price * discount);
         } else if (item.productID === 'FR1' && (item.quantity % 2 == 0)) {
             sum += (item.price * item.quantity) / 2;
+            item.total = (item.price * item.quantity) / 2;
         } else if (item.productID === 'FR1' && item.quantity === 1) {
             sum += item.price;
+            item.total = item.price;
         };
         if (item.productID === 'AP1' && item.quantity >= 3) {
             sum += 4.5 * item.quantity;
+            item.total = 4.5 * item.quantity;
         } else if (item.productID === 'AP1' && item.quantity <= 2) {
             sum += item.price * item.quantity;
+            item.total =  item.price * item.quantity;
         };
         if (item.productID === 'CF1') {
           sum += item.price * item.quantity;
+          item.total =  item.price * item.quantity;
         };
     }
     $scope.products = [
@@ -50,11 +56,11 @@ app.controller('CartCtrl', ['$scope', function($scope){
     $scope.add = function(item){
         if (item.quantity === 0){
             item.quantity += 1;
-            item.total += item.price;
+            // item.total += item.price;
             $scope.items.push(item);
         } else {
             item.quantity += 1;
-            item.total += item.price;
+            // item.total += item.price;
         }
     };
     $scope.total = function(){
@@ -76,11 +82,11 @@ app.controller('CartCtrl', ['$scope', function($scope){
       });
       $scope.items = [];
     };
-    $scope.discount = function(){
-      var itemsTotal = 0;
-      $scope.items.forEach(function(item){
-          itemsTotal += item.total;
-      })
-      return itemsTotal - $scope.total();
-    }
+    // $scope.discount = function(){
+    //   var itemsTotal = 0;
+    //   $scope.items.forEach(function(item){
+    //       itemsTotal += item.total;
+    //   })
+    //   return itemsTotal - $scope.total();
+    // }
 }])
